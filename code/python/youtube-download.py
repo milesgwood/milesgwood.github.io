5c85fc0a0bf8a672932953b43e9b1826e210abd6
milesgwood <miles.gwood@gmail.com>
2018-04-13 15:53:33 -0400

    Instructions for setting up and installing the script and setting it up

--- code/python/youtube-download.py
+++ code/python/youtube-download.py
@@ -1,6 +1,20 @@
 from __future__ import unicode_literals
 import youtube_dl, sys, pyperclip, threading
 
+# 1. Use Python 36 from CMD using python command or F5 in Atom
+# 2. `pip install youtube_dl`
+# 3. `pip install pyperclip`
+# 4. [Install FFmpeg windows](https://github.com/adaptlearning/adapt_authoring/wiki/Installing-FFmpeg)
+# 5. Add ffmpeg to path `C:\Program Files\ffmpeg-20180412-8d381b5-win64-static\bin`
+
+# Getting this working on Mac Machines
+# You'll may need to upgrade TLS to v1.2 - best to just use python3 https://news.ycombinator.com/item?id=13539034
+# 1. Install HomeBrew ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# 2. brew install youtube-dl
+# 3. pip3.6 install youtube-dl
+# 4. pip3.6 install pyperclip
+# 5. brew install ffmpeg
+
 def get_all_command_line_ars():
     if len(sys.argv) > 1:
         # Get address from command line.
